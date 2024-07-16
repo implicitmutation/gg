42fddf614c35773e11938df413c4619f133912e3
templateconnectio <templateconnectio@gmail.com>
2024-07-16 19:19:47 +0000

    ui

--- template/idx-template.nix
+++ template/idx-template.nix
@@ -4,17 +4,21 @@ idx-template \
   /home/user/gg/template \
   --output-dir /home/user/gg/test -a '{}'
 */
-{pkgs, ... }: {  
+{pkgs, language ? "js", tos ? "false" ... }: {  
   packages = [
     pkgs.nodejs
+    pkgs.go
+    pkgs.util-linux
   ];
   bootstrap = ''
     mkdir "$out"
     mkdir "$out"/.idx
     cp ${./dev.nix} "$out"/.idx/dev.nix
     cp ${./README.md} "$out"/README.md
-    npm i -D genkit
-    npx genkit init --non-interactive -p other -m googleai
+    env -C "$out" go mod init genkit
+    env -C "$out" npm i -D genkit
+    echo $(npx genkit --version)
+    env -C "$out" npx genkit init --non-interactive -p other -m googleai
     chmod -R u+w "$out"
   '';
 }
